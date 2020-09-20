0e49db3b693b26ae2147cb45fcb4594856b05500
couplesit <inseok.yeon@gmail.com>
2020-09-20 09:01:33 +0900

    성경 타이핑

---
+++ pages/python/files/hello.py
@@ -0,0 +1,18 @@
+print("Hello Coding Python")
+
+# 파이썬 규칙
+# 클래스 작성시 대문자로 시작하여 낙타식 표기 PrintHello
+# 변수, 함수 작성시 소문자로 시작하여 스네이크식 표기 print_hello
+
+# 하나만 출력합니다.
+print("# 하나만 출력합니다.")
+print("Hello Python Programming...")
+print()
+
+# 여러 개를 출력합니다.
+print("# 여러 개를 출력합니다.")
+print(10, 20, 30, 40, 50)
+print("안녕하세요", "저의", "이름은", "홍길동입니다!")
+print()
+
+
